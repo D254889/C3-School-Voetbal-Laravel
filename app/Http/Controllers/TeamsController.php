06ba31ca70a99606ddc8c41128f4d7e2d1313670
D254889 <D254889@edu.curio.nl>
2024-11-25 11:24:37 +0100

    TeamsController

--- app/Http/Controllers/TeamsController.php
+++ app/Http/Controllers/TeamsController.php
@@ -7,5 +7,44 @@ use App\Models\Team;
 
 class TeamsController extends Controller
 {
-    //
+    public function index()
+    {
+        $teams = Teams::all();
+        return view("teams.index")->with("teams", $teams);
+    }
+
+    public function create()
+    {
+        return view('teams.create');
+    }
+
+    public function store(Request $request)
+    {
+        $newTeams = new Team;
+        $newTeams->name = $request->name;
+        $newTeams->age = $request->age;
+        $newTeams->email = $request->email;
+        $newTeams->save();
+        return redirect()->route('customers.index');
+    }
+
+    public function edit(Customer $customer)
+    {
+        return view('customers.edit')->with('customer', $customer);
+    }
+
+    public function update(Customer $customer, Request $request)
+    {
+        $customer->name = $request->name;
+        $customer->age = $request->age;
+        $customer->email = $request->email;
+        $customer->save();
+        return redirect()->route('customers.index');
+    }
+
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+        return redirect()->route('customers.index');
+    }
 }
