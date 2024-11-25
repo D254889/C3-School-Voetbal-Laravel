0dd3c4f74e63b1b84184537688e78d6cba08f02b
5oepkip <Finnnika@gmail.com>
2024-11-25 11:26:06 +0100

    home begin
    home pagina met oefen text

---
+++ resources/views/home.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>schoolvoetbal</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="home">home</a>
+            <a href="teams">teams</a>
+            <a href="player">//</a>
+            logout
+        </nav>
+    </header>
+    <main>
+        <div class="wrapper">
+            <h1>School voetbal</h1>
+            <div class="topTeams">
+                <h2>Top 5</h2>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                </ul>
+            </div>
+            <img src="" alt="">
+
+            <a href="wedstrijden">
+                <h2>wedstrijden</h2>
+                <ul>
+                    <li>1 team vs team</li>
+                    <li>2 team vs team</li>
+                    <li>3 team vs team</li>
+                    <li>4 team vs team</li>
+                    <li>5 team vs team</li>
+                </ul>
+            </a>
+            //if logged in
+            <div class="team">
+                <h2>team</h2>
+                <ul>
+                    <li>player name</li>
+                    <li>player name</li>
+                    <li>player name</li>
+                    <li>player name</li>
+                    <li>player name</li>
+                    <li>player name</li>
+                    <li>player name</li>
+                    <li>player name</li>
+                    <li>player name</li>
+                    <li>player name</li>
+                </ul>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
+
