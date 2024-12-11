5b800fd9bf83d89a68ba7d06420807c7a55e7dc1
Fietsbel12 <Lotusk20@outlook.com>
2024-12-11 23:40:46 +0100

    index styling + css added

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,12 +9,45 @@
     <title>schoolvoetbal</title>
 </head>
 <body>
+    <div class="body-bg"></div>
     <header>
         <x-navbar />
     </header>
     <main>
         <div class="h1title"><h1>Schoolvoetbal</h1></div>
-        <div class="logoFoto"><img src="{{asset('/style/img/logoBlauw.png')}}"/></div>
+        <div class="container">
+            <div class="box top-five">
+                <h2>Top 5</h2>
+            </div>
+            <div class="box image">
+                <img src="{{asset('/style/img/logoBlauw.png')}}"/>
+            </div>
+
+            <div class="wedstrijden-box">
+                <a href="{{ route('wedstrijdschema') }}" class="btn">Naar het Wedstrijdschema</a>
+            </div>
+
+            <div class="box my-team">
+                <h2>Mijn team:</h2>
+                <table>
+                    <thead>
+                        <th>Team 1</th>
+                        <th>Team 2</th>
+                    </thead>
+                    {{--
+                    <tbody>
+                        @foreach ($teams as $team )
+                            <tr>
+                                <td>{{$team->team1_id}}</td>
+                                <td>{{$team->teams2_id}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                    --}}
+                </table>
+            </div>
+        </div>
+
         {{$slot}}
     </main>
     <footer>
