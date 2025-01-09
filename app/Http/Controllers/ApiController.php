1e7457c300d1ae508eb663e888b953e19d7eb652
Fietsbel12 <Lotusk20@outlook.com>
2025-01-09 17:00:51 +0100

    API commit

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class ApiController extends Controller
 {
     public function getMatches(){
-        $matches = Contest::select('team1_id', 'teams2_id', 'team1_score', 'team2_score')->get();
+        $matches = Contest::select('team1_id', 'teams2_id', 'time')->get();
     return response()->json($matches);
     }
 }
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer">
     <footer>
-        <p>Made by, @Reinout van Schilt, @Lotus Koning, @Finn Langeveld <script>document.write(new Date().getFullYear());</script></p>
+        <p>Made by, @Reinout van Schilt, @Lotus Koning, @Finn Langeveld, 2024</p>
     </footer>
 </div>
